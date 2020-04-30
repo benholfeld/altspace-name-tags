572cc4de9e186c63b6f079e936987b4f0b96eeaf
Dave Arendash <david.j.arendash@accenture.com>
2020-04-30 00:22:48 -0700

    Initial commit. Adapted from the 'Wear a Hat' demo

---
+++ src/app.ts
@@ -0,0 +1,301 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import * as MRE from '@microsoft/mixed-reality-extension-sdk';
+import { double } from '@microsoft/mixed-reality-extension-sdk/built/math/types';
+
+/**
+ * The structure of a name tag entry in the name tag database.
+ */
+type NameTagDescriptor = {
+	displayName: string;
+	resourceName: string;
+	scale: {
+		x: number;
+		y: number;
+		z: number;
+	};
+	rotation: {
+		x: number;
+		y: number;
+		z: number;
+	};
+	position: {
+		x: number;
+		y: number;
+		z: number;
+	};
+};
+
+/**
+ * The structure of the name tag database.
+ */
+type NameTagDatabase = {
+	[key: string]: NameTagDescriptor;
+};
+
+// Load the database of name tag.
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const NameTagDatabase: NameTagDatabase = require('../public/nametags.json');
+
+/**
+ * NameTag Application - Showcasing avatar attachments.
+ */
+export default class NameTag {
+	// Container for preloaded name tag prefabs.
+	private assets: MRE.AssetContainer;
+	private prefabs: { [key: string]: MRE.Prefab } = {};
+	// Container for instantiated name tags.
+	private attachedNameTags = new Map<MRE.Guid, MRE.Actor>();
+
+	/**
+	 * Constructs a new instance of this class.
+	 * @param context The MRE SDK context.
+	 * @param baseUrl The baseUrl to this project's `./public` folder.
+	 */
+	constructor(private context: MRE.Context, private baseUrl: string) {
+		this.assets = new MRE.AssetContainer(context);
+		// Hook the context events we're interested in.
+		this.context.onStarted(() => this.started());
+		this.context.onUserJoined(user => this.userJoined(user));
+		this.context.onUserLeft(user => this.userLeft(user));
+	}
+
+	/**
+	 * Called when a NameTag application session starts up.
+	 */
+	private async started() {
+		// Preload all the name tag models.
+		await this.preloadNameTags();
+		// Show the name tag menu.
+		this.showNameTagMenu();
+	}
+
+	/**
+	 * Called when a user leaves the application (probably left the Altspace world where this app is running).
+	 * @param user The user that left the building.
+	 */
+	private userJoined(user: MRE.User) {
+		// Create default name tag
+		this.wearNameTag(MRE.Color3.White(), user.id);
+	}
+
+	/**
+	 * Called when a user leaves the application (probably left the Altspace world where this app is running).
+	 * @param user The user that left the building.
+	 */
+	private userLeft(user: MRE.User) {
+		// If the user was wearing a name tag, destroy it. Otherwise it would be
+		// orphaned in the world.
+		this.removeNameTags(user);
+	}
+
+	/**
+	 * Show a menu of name tag options.
+	 */
+	private showNameTagMenu() {
+		// Create a parent object for all the menu items.
+		const menu = MRE.Actor.Create(this.context, {});
+		let y = 0.3;
+		const x = 0.0;
+
+		// Create menu button
+		const buttonMesh = this.assets.createBoxMesh('button', 0.3, 0.3, 0.01);
+
+		this.createColorButton (buttonMesh, menu, x, y, "Red", MRE.Color3.Red());
+		y = y + 0.5;
+		this.createColorButton (buttonMesh, menu, x, y, "Blue", MRE.Color3.Blue());
+		y = y + 0.5;
+		this.createColorButton (buttonMesh, menu, x, y, "Green", MRE.Color3.Green());
+		y = y + 0.5;
+		this.createColorButton (buttonMesh, menu, x, y, "Black", MRE.Color3.Black());
+		y = y + 0.5;
+		this.createColorButton (buttonMesh, menu, x, y, "White", MRE.Color3.White());
+		/*
+		// Loop over the name tag database, creating a menu item for each entry.
+		for (const nameTagId of Object.keys(NameTagDatabase)) {
+			// Create a clickable button.
+			const button = MRE.Actor.Create(this.context, {
+				actor: {
+					parentId: menu.id,
+					name: nameTagId,
+					appearance: { meshId: buttonMesh.id },
+					collider: { geometry: { shape: MRE.ColliderType.Auto } },
+					transform: {
+						local: { position: { x: 0, y, z: 0 } }
+					}
+				}
+			});
+
+			// Set a click handler on the button.
+			button.setBehavior(MRE.ButtonBehavior)
+				.onClick(user => this.wearNameTag(nameTagId, user.id));
+
+			// Create a label for the menu entry.
+			MRE.Actor.Create(this.context, {
+				actor: {
+					parentId: menu.id,
+					name: 'label',
+					text: {
+						contents: NameTagDatabase[nameTagId].displayName,
+						height: 0.5,
+						anchor: MRE.TextAnchorLocation.MiddleLeft
+					},
+					transform: {
+						local: { position: { x: 0.5, y, z: 0 } }
+					}
+				}
+			});
+			y = y + 0.5;
+		}
+*/
+		// Create a label for the menu title.
+		MRE.Actor.Create(this.context, {
+			actor: {
+				parentId: menu.id,
+				name: 'label',
+				text: {
+					contents: ''.padStart(8, ' ') + "Name Tag",
+					height: 0.8,
+					anchor: MRE.TextAnchorLocation.MiddleCenter,
+					color: MRE.Color3.Blue()
+				},
+				transform: {
+					local: { position: { x: 0.5, y: y + 0.5, z: 0 } }
+				}
+			}
+		});
+	}
+
+	private createColorButton(buttonMesh: MRE.Mesh, menu: MRE.Actor, x: double, y: double,
+		colorName: string, col: MRE.Color3) {
+		// Create a clickable button.
+		const button = MRE.Actor.Create(this.context, {
+			actor: {
+				parentId: menu.id,
+				name: colorName,
+				appearance: { meshId: buttonMesh.id },
+				collider: { geometry: { shape: MRE.ColliderType.Auto } },
+				transform: {
+					local: { position: { x, y, z: 0 } }
+				}
+			}
+		});
+
+		// Set a click handler on the button.
+		button.setBehavior(MRE.ButtonBehavior)
+				.onClick(user => this.wearNameTag(col, user.id));
+
+		// Create a label for the menu entry.
+		MRE.Actor.Create(this.context, {
+			actor: {
+				parentId: menu.id,
+				name: 'label',
+				text: {
+					contents: colorName,
+					height: 0.5,
+					color: col,
+					anchor: MRE.TextAnchorLocation.MiddleLeft
+				},
+				transform: {
+					local: { position: { x: x + 0.5, y, z: 0 } }
+				}
+			}
+		});
+	}
+
+
+	/**
+	 * Preload all name tag resources. This makes instantiating them faster and more efficient.
+	 */
+	private preloadNameTags() {
+		// Loop over the name tag database, preloading each name tag resource.
+		// Return a promise of all the in-progress load promises. This
+		// allows the caller to wait until all name tag are done preloading
+		// before continuing.
+		return Promise.all(
+			Object.keys(NameTagDatabase).map(nameTagId => {
+				const nameTagRecord = NameTagDatabase[nameTagId];
+				if (nameTagRecord.resourceName) {
+					return this.assets.loadGltf(
+						`${this.baseUrl}/${nameTagRecord.resourceName}`)
+						.then(assets => {
+							this.prefabs[nameTagId] = assets.find(a => a.prefab !== null) as MRE.Prefab;
+						})
+						.catch(e => MRE.log.error("app", e));
+				} else {
+					return Promise.resolve();
+				}
+			}));
+	}
+
+	/**
+	 * Instantiate a nametag and attach it to the avatar's body.
+	 * @param nameTagId The id of nametag in the nametag database.
+	 * @param userId The id of the user we will attach the nametag to.
+	 */
+	private wearNameTag(/*nameTagId: string*/ col: MRE.Color3, userId: MRE.Guid) {
+		// If the user is wearing a name tag, destroy it.
+		this.removeNameTags(this.context.user(userId));
+		/*
+		const nameTagRecord = NameTagDatabase[nameTagId];
+
+		// If the user selected 'none', then early out.
+		if (!nameTagRecord.resourceName) {
+			return;
+		}
+*/
+		// Create the nametag model and attach it to the avatar's body.
+		/*		this.attachedNameTags.set(userId, MRE.Actor.CreateFromPrefab(this.context, {
+			prefabId: this.prefabs[nameTagId].id,
+			actor: {
+				transform: {
+					local: {
+						position: nameTagRecord.position,
+						rotation: MRE.Quaternion.FromEulerAngles(
+							nameTagRecord.rotation.x * MRE.DegreesToRadians,
+							nameTagRecord.rotation.y * MRE.DegreesToRadians,
+							nameTagRecord.rotation.z * MRE.DegreesToRadians),
+						scale: nameTagRecord.scale,
+					}
+				},
+				attachment: {
+					attachPoint: 'head',
+					userId
+				}
+			}
+		}));
+*/
+		// Create a label for the menu title.
+		this.attachedNameTags.set(userId, MRE.Actor.Create(this.context, {
+			actor: {
+				text: {
+					contents: this.context.user(userId).name,
+					height: 0.1,
+					anchor: MRE.TextAnchorLocation.MiddleCenter,
+					color: col
+				},
+				transform: {
+					local: {
+						position: { x: 0, y: 0, z: .15 },
+						rotation: MRE.Quaternion.FromEulerAngles(
+							0 * MRE.DegreesToRadians,
+							180 * MRE.DegreesToRadians,
+							0 * MRE.DegreesToRadians),
+					}
+				},
+				attachment: {
+					attachPoint: 'spine-top',
+					userId
+				}
+			}
+		}));
+	}
+
+	private removeNameTags(user: MRE.User) {
+		if (this.attachedNameTags.has(user.id)) { this.attachedNameTags.get(user.id).destroy(); }
+		this.attachedNameTags.delete(user.id);
+	}
+}
